ee703639c848f05c418609b3bd7eb9d7550dd0f5
tonox24 <andishrtono@gmail.com>
2026-09-16 06:52:22 +0700

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { PrismaModule } from './prisma.module';
 import { RbacModule } from './rbac/rbac.module';
 import { RbacTestModule } from './rbac-test/rbac-test.module';
 import { ProgramModule } from './program/program.module';
+import { CampaignModule } from './campaign/campaign.module';
 
 @Module({
   imports: [
