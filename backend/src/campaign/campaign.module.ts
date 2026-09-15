288daca349e1a41c18d7c969d4a57f8e30e33531
tonox24 <andishrtono@gmail.com>
2026-09-16 06:51:30 +0700

    Create campaign.module.ts

---
+++ backend/src/campaign/campaign.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+
+import { CampaignController } from './campaign.controller';
+import { CampaignService } from './campaign.service';
+
+@Module({
+  controllers: [
+    CampaignController,
+  ],
+
+  providers: [
+    CampaignService,
+  ],
+
+  exports: [
+    CampaignService,
+  ],
+})
+export class CampaignModule {}
