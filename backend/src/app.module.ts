698a908d7c352ee81fdbc6afd71e8f7bae5b90a9
tonox24 <andishrtono@gmail.com>
2026-09-16 06:55:11 +0700

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,38 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { HealthController } from './health.controller';
+
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma.module';
+
 import { RbacModule } from './rbac/rbac.module';
 import { RbacTestModule } from './rbac-test/rbac-test.module';
+
 import { ProgramModule } from './program/program.module';
 import { CampaignModule } from './campaign/campaign.module';
 
 @Module({
   imports: [
-  ConfigModule.forRoot({
-    isGlobal: true,
-  }),
-  PrismaModule,
-  RbacModule,
-  AuthModule,
-  RbacTestModule,
-  ProgramModule,
-],
-  controllers: [HealthController],
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    PrismaModule,
+
+    RbacModule,
+
+    AuthModule,
+
+    RbacTestModule,
+
+    ProgramModule,
+
+    CampaignModule,
+  ],
+
+  controllers: [
+    HealthController,
+  ],
 })
 export class AppModule {}
