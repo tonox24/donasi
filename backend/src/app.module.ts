0aeaa6e630684a4c86d8e31b08baa21596b2be8b
tonox24 <andishrtono@gmail.com>
2026-09-16 07:43:14 +0700

    Add campaign category module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-
 import { HealthController } from './health.controller';
-
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma.module';
-
 import { RbacModule } from './rbac/rbac.module';
 import { RbacTestModule } from './rbac-test/rbac-test.module';
-
 import { ProgramModule } from './program/program.module';
 import { CampaignModule } from './campaign/campaign.module';
 import { CampaignCategoryModule } from './campaign-category/campaign-category.module';
