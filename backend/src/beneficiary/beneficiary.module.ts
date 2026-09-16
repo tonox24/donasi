87311af592575e2c9fb055aaa2abee2e15695f04
tonox24 <andishrtono@gmail.com>
2026-09-16 11:33:47 +0700

    Create beneficiary.module.ts

---
+++ backend/src/beneficiary/beneficiary.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+
+import { BeneficiaryController } from './beneficiary.controller';
+import { BeneficiaryService } from './beneficiary.service';
+
+@Module({
+  controllers: [
+    BeneficiaryController,
+  ],
+
+  providers: [
+    BeneficiaryService,
+  ],
+
+  exports: [
+    BeneficiaryService,
+  ],
+})
+export class BeneficiaryModule {}
