6b65de62ccac9178250694d89f52c97bb9c59d0f
tonox24 <andishrtono@gmail.com>
2026-09-18 07:33:12 +0700

    Update receipt.module.ts

--- backend/src/receipt/receipt.module.ts
+++ backend/src/receipt/receipt.module.ts
@@ -4,8 +4,16 @@ import { ReceiptController } from './receipt.controller';
 import { ReceiptService } from './receipt.service';
 
 @Module({
-  controllers: [ReceiptController],
-  providers: [ReceiptService],
-  exports: [ReceiptService],
+  controllers: [
+    ReceiptController,
+  ],
+
+  providers: [
+    ReceiptService,
+  ],
+
+  exports: [
+    ReceiptService,
+  ],
 })
 export class ReceiptModule {}
