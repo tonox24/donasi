3ced9bc2b6bb2da9624a881fd5124026612c9ef6
tonox24 <andishrtono@gmail.com>
2026-09-18 07:14:03 +0700

    Update payment.module.ts

--- backend/src/payment/payment.module.ts
+++ backend/src/payment/payment.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
+import { ReceiptModule } from '../receipt/receipt.module';
 
 @Module({
-  controllers: [PaymentController],
-  providers: [PaymentService],
-  exports: [PaymentService],
+  imports: [
+    ReceiptModule,
+  ],
+  controllers: [
+    PaymentController,
+  ],
+  providers: [
+    PaymentService,
+  ],
+  exports: [
+    PaymentService,
+  ],
 })
 export class PaymentModule {}
