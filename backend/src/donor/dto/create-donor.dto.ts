dd8e7a82403d2a7d3975f10a438d25cb50c2e063
tonox24 <andishrtono@gmail.com>
2026-09-19 08:13:11 +0700

    Create create-donor.dto.ts

---
+++ backend/src/donor/dto/create-donor.dto.ts
@@ -0,0 +1,49 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MinLength,
+} from 'class-validator';
+
+import { DonorType } from '@prisma/client';
+
+export class CreateDonorDto {
+  @IsOptional()
+  @IsUUID()
+  userId?: string;
+
+  @IsString()
+  @MinLength(2)
+  fullName!: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsString()
+  city?: string;
+
+  @IsOptional()
+  @IsString()
+  country?: string;
+
+  @IsOptional()
+  @IsEnum(DonorType)
+  donorType?: DonorType;
+
+  @IsOptional()
+  @IsBoolean()
+  isAnonymous?: boolean;
+}
