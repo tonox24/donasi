bcfb8e53869e67eb7dbe6da20fb56f0d919eff3e
tonox24 <andishrtono@gmail.com>
2026-09-22 14:51:04 +0700

    Create create-qurban-saving.dto.ts

---
+++ backend/src/qurban/dto/create-qurban-saving.dto.ts
@@ -0,0 +1,56 @@
+import {
+  IsDateString,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+  Min,
+} from 'class-validator';
+
+import {
+  QurbanAnimalType,
+  QurbanSavingFrequency,
+} from '@prisma/client';
+
+export class CreateQurbanSavingDto {
+  @IsUUID()
+  donorId!: string;
+
+  @IsEnum(QurbanAnimalType)
+  qurbanAnimalType!: QurbanAnimalType;
+
+  @IsNumber()
+  @Min(1)
+  targetAmount!: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  contributionAmount?: number;
+
+  @IsEnum(QurbanSavingFrequency)
+  frequency!: QurbanSavingFrequency;
+
+  @IsDateString()
+  startDate!: string;
+
+  @IsDateString()
+  targetDate!: string;
+
+  @IsOptional()
+  @IsString()
+  distributionLocation?: string;
+
+  @IsOptional()
+  @IsString()
+  pekurbanName?: string;
+
+  @IsOptional()
+  @IsString()
+  notes?: string;
+
+  @IsOptional()
+  @IsString()
+  currency?: string;
+}
