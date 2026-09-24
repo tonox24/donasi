0ded7ac22930d92229a87605ac93b646dc2a99de
tonox24 <andishrtono@gmail.com>
2026-09-24 11:44:22 +0700

    Add Qurban animal management workflow

--- backend/src/qurban/qurban.module.ts
+++ backend/src/qurban/qurban.module.ts
@@ -2,18 +2,23 @@ import { Module } from '@nestjs/common';
 
 import { QurbanController } from './qurban.controller';
 import { QurbanService } from './qurban.service';
+import { QurbanAnimalController } from './qurban-animal.controller';
+import { QurbanAnimalService } from './qurban-animal.service';
 
 @Module({
   controllers: [
     QurbanController,
+    QurbanAnimalController,
   ],
 
   providers: [
     QurbanService,
+    QurbanAnimalService,
   ],
 
   exports: [
     QurbanService,
+    QurbanAnimalService,
   ],
 })
 export class QurbanModule {}
