f41549aa06059eb2622f98a94f7631379ea1223c
tonox24 <andishrtono@gmail.com>
2026-09-24 14:31:23 +0700

    Register Qurban documentation module

--- backend/src/qurban/qurban.module.ts
+++ backend/src/qurban/qurban.module.ts
@@ -2,23 +2,30 @@ import { Module } from '@nestjs/common';
 
 import { QurbanController } from './qurban.controller';
 import { QurbanService } from './qurban.service';
+
 import { QurbanAnimalController } from './qurban-animal.controller';
 import { QurbanAnimalService } from './qurban-animal.service';
+
+import { QurbanDocumentationController } from './qurban-documentation.controller';
+import { QurbanDocumentationService } from './qurban-documentation.service';
 
 @Module({
   controllers: [
     QurbanController,
     QurbanAnimalController,
+    QurbanDocumentationController,
   ],
 
   providers: [
     QurbanService,
     QurbanAnimalService,
+    QurbanDocumentationService,
   ],
 
   exports: [
     QurbanService,
     QurbanAnimalService,
+    QurbanDocumentationService,
   ],
 })
 export class QurbanModule {}
